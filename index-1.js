65118ed70a9f2706961c3f684d3488dac9565781
GabiPostin <begere2009@gmail.com>
2025-06-29 02:09:48 +0300

    Finish_First_Index

---
+++ index-1.js
@@ -0,0 +1,16 @@
+const containerLeftH2 = document.getElementById("containerL-h2");
+containerLeftH2.textContent = "SO, YOU WANT TO TRAVEL TO";
+
+const containerLeftH1 = document.getElementById("containerL-h1");
+containerLeftH1.textContent = "SPACE";
+
+const containerLeftP = document.getElementById("containerL-p");
+containerLeftP.textContent = "Let’s face it; if you want to go to space, you might as well\n genuinely go to outer space and not hover kind of on the\n edge of it. Well sit back, and relax because we’ll give you a\n truly out of this world experience!";
+
+const exploreButton = document.getElementById("exploreButton");
+
+exploreButton.src = "./images/Group.png";
+
+exploreButton.addEventListener("click", () => {
+  window.location.href = "index-2";
+});
